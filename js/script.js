4df16dee7968a8d048448df351886dc48ce84ad0
Radyslav Dmytrenko <sheogoradik@gmail.com>
2024-03-16 12:17:07 +0200

    Added respinsive

--- js/script.js
+++ js/script.js
@@ -2,5 +2,19 @@ $(document).ready(function(){
     $('.slider__balls').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
     });
 });
